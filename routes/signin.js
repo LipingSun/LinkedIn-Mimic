8bc92439bb1961697f5447ed7304b705899a7b30
LipingSun <liping.sun@sjsu.edu>
2015-03-15 16:03:25 -0700

    encrypt password with crypto.pbkdf2

--- routes/signin.js
+++ routes/signin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mysql = require('./mysql');
 var time = require('./time');
 var session = require('express-session');
+var crypto = require('crypto');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
@@ -10,22 +11,23 @@ router.post('/', function(req, res, next) {
     mysql.query(sql, function (err, data) {
         //console.log(data[0]);
         if (data[0]) {
-            if (req.body.password === data[0].password) {
-                req.session.regenerate(function (err) {
-                    req.session.user = data[0];
-                    res.location('/home');
-                    res.end('Login Success');
-                    console.log('login time: ' + time());
-                    var sql = 'UPDATE user SET last_login_time=' + mysql.escape(time()) + ' WHERE user_id='+ req.session.user.user_id;
-                    mysql.query(sql, function (err, data) {
-                        if (err) console.log("DB ERROR: " + err.message);
+            console.log('pw: ' + data[0].password);
+            crypto.pbkdf2(req.body.password, data[0].salt, 10000, 32, 'sha256', function(err, key) {
+                if (key.equals(data[0].password)) {
+                    req.session.regenerate(function (err) {
+                        req.session.user = data[0];
+                        res.location('/home');
+                        res.end('Login Success');
+                        console.log('login time: ' + time());
+                        var sql = 'UPDATE user SET last_login_time=' + mysql.escape(time()) + ' WHERE user_id=' + req.session.user.user_id;
+                        mysql.query(sql, function (err, data) {
+                            if (err) console.log("DB ERROR: " + err.message);
+                        });
                     });
-                    res.location('/home');
-                    res.end('Login Success');
-                });
-            } else {
-                res.send('Wrong Password');
-            }
+                } else {
+                    res.send('Wrong Password');
+                }
+            });
         } else {
             res.send('Can\'t find this user');
         }
--- routes/signup.js
+++ routes/signup.js
@@ -2,29 +2,45 @@ var express = require('express');
 var mysql = require('./mysql');
 var time = require('./time');
 var session = require('express-session');
+var crypto = require('crypto')
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
     console.log(req.body);
-    var sql = 'INSERT INTO user (email, password, firstname, lastname, last_login_time) VALUES ('
-        + mysql.escape(req.body.email) + ', '
-        + mysql.escape(req.body.password) + ', '
-        + mysql.escape(req.body.firstname) + ', '
-        + mysql.escape(req.body.lastname) + ', '
-        + mysql.escape(time()) + ');'
-        + 'SELECT * FROM user WHERE email=' + mysql.escape(req.body.email);
-    mysql.query(sql, function (err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            req.session.regenerate(function (err) {
-                req.session.user = data[1][0];
-                req.session.user.last_login_time = null;
-                res.location('/home');
-                res.end('Signup Success');
+
+    crypto.randomBytes(16, function(ex, salt) {
+        if (ex) throw ex;
+        console.log('Have %d bytes of random salt: %s', salt.length, salt.toString('hex'));
+        crypto.pbkdf2(req.body.password, salt, 10000, 32, 'sha256', function(err, key) {
+            if (err) throw err;
+
+            console.log('Have %d bytes of key: %s', key.length, key.toString('hex'));
+
+            var sql = 'INSERT INTO user (email, password, salt, firstname, lastname, last_login_time) VALUES ('
+                + mysql.escape(req.body.email) + ', '
+                + mysql.escape(key) + ', '
+                + mysql.escape(Buffer(salt, 'binary')) + ', '
+                + mysql.escape(req.body.firstname) + ', '
+                + mysql.escape(req.body.lastname) + ', '
+                + mysql.escape(time()) + ');'
+                + 'SELECT * FROM user WHERE email=' + mysql.escape(req.body.email);
+            mysql.query(sql, function (err, data) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    req.session.regenerate(function (err) {
+                        req.session.user = data[1][0];
+                        req.session.user.last_login_time = null;
+                        res.location('/home');
+                        res.end('Signup Success');
+                    });
+                }
             });
-        }
+
+        });
     });
+
+
 });
 
 module.exports = router;
